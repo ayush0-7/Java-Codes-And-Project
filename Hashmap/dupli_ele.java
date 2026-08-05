60fbc218f200fa760e6ed3f4f4739652ef8cdf35
ayush <ayushm8896@gmail.com>
2026-08-05 09:59:35 +0530

    java codes

--- Hashmap/dupli_ele.java
+++ Hashmap/dupli_ele.java
@@ -2,14 +2,14 @@ import java.util.HashMap;
 
 public class dupli_ele {
     public static void main(String[] args) {
-        int a[]={1, 2, 3, 2, 4, 1, 5};
-        HashMap<Integer,Integer>mp=new HashMap<>();
+        int a[] = { 1, 2, 3, 2, 4, 1, 5, 5, 4 };
+        HashMap<Integer, Integer> mp = new HashMap<>();
         for (int i : a) {
-            mp.put(i, mp.getOrDefault(i, 0)+1);
+            mp.put(i, mp.getOrDefault(i, 0) + 1);
         }
         for (var i : mp.entrySet()) {
-            if (i.getValue()>1) {
-                System.out.println(i);
+            if (i.getValue() == 1) {
+                System.out.println(i.getKey());
             }
         }
     }
